45a9e0164b79a37942c4596913b4c4249c19634b
Maalim98 <ashrafzani585@gmail.com>
2023-11-16 12:26:50 +0300

    create a middle waire& a function 2 handle error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,10 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
 
 
 
-export const signup = async (req,res) =>{
+export const signup = async (req,res,next) =>{
     const { username ,email,password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
 
@@ -12,7 +13,7 @@ export const signup = async (req,res) =>{
         await newUser.save();
         restart.status(201).json('user created succesfully!');
     }catch(error){
-        res.status(500).json(error.message);
+        next(error);
     }
 
 
