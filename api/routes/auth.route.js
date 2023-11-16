4d4d9595c1d9071c4aec320e317dfdda34b54fa8
Maalim98 <ashrafzani585@gmail.com>
2023-11-16 19:30:13 +0300

    create signin api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,11 @@
 import  express from "express";
 import { signup } from "../controllers/auth.controller.js";
+import SignIn from "../../client/src/pages/SignIn.jsx";
 
 
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/SigIn", SignIn);
 
 export default router;
